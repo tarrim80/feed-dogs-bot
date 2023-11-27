6d0dcc3e7df230eb42a08bc33076164057009819
Roman Tarasenko <tarrim80@yandex.ru>
2023-11-27 13:43:57 +0600

    Fix chat_id

--- core/handlers/apsched.py
+++ core/handlers/apsched.py
@@ -1,11 +1,12 @@
 from aiogram import Bot
 
 import core.keyboards as kb
+from core.database.requests import get_chat_id
 
 
 async def feed_dogs_time(bot: Bot) -> None:
     await bot.send_message(
-        -1001669568306,
-        "Пора покормить собачек!",
+        chat_id=await get_chat_id(),
+        text="Пора покормить собачек!",
         reply_markup=await kb.i_will_feeding(),
     )
